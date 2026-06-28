2be07d9f8e8b675a9884c224390fb4aae1a2b48d
josue <heribertojosue123@gmail.com>
2026-06-27 23:44:30 -0600

    Feature: Poblar sección EVENTOS con 30 canales deportivos IPTV (ESPN, FOX, BeIN)

---
+++ eventos_database.js
@@ -0,0 +1,152 @@
+const eventosDatabase = [
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/Lv53nh4.png",
+    "url": "https://30a-tv.com/feeds/vidaa/golf.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/oAtcxNZ.png",
+    "url": "http://88.212.15.27/live/test_rtvs_sport_hevc/playlist.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/ZhkZzLf.png",
+    "url": "https://rnttwmjcin.turknet.ercdn.net/lcpmvefbyo/aspor/aspor.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/V6Kaqha.png",
+    "url": "https://raycom-accdn-firetv.amagi.tv/playlist.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/U8cHMOt.png",
+    "url": "https://webstream.multistream.it/memfs/e2cb3629-c1a2-495b-b43a-9eb386f04ed8.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/SVV5xHC.png",
+    "url": "https://live20.bozztv.com/dvrfl05/gin-adjara/index.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/pxFamLr.png",
+    "url": "https://strhls.streamakaci.tv/ortb/ortb2-multi/playlist.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i0.wp.com/africa24tv.com/wp-content/uploads/2023/12/LOGO-AFRICASPORT-4-HD-sans-fond.png?fit=512%2C107&ssl=1",
+    "url": "https://africa24.vedge.infomaniak.com/livecast/ik:africa24sport/manifest.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/DrrlxTO.png",
+    "url": "https://imn-live.esite-lab.com/hls/iraqia-sports-1.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/vhMPYIW.png",
+    "url": "https://dev.aftermind.xyz/edge-hls/unitrust/alfasports/index.m3u8?token=8TXWzhY3h6jrzqEqx"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/6RGNGsM.png",
+    "url": "https://liveeu-gcp.alkassdigital.net/alkass5-p/main.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/iDL65Wu.png",
+    "url": "https://liveeu-gcp.alkassdigital.net/alkass4-p/main.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/10mmlha.png",
+    "url": "https://liveeu-gcp.alkassdigital.net/alkass1-p/main.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/3eyHP3S.png",
+    "url": "https://liveeu-gcp.alkassdigital.net/alkass7-p/main.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://shoof.alkass.net/assets/images/shoof.png",
+    "url": "https://liveeu-gcp.alkassdigital.net/shooflive/main.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://shoof.alkass.net/assets/images/shoof2.png",
+    "url": "https://liveeu-gcp.alkassdigital.net/shooflive2/main.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/CrPSPSC.png",
+    "url": "https://liveeu-gcp.alkassdigital.net/alkass6-p/main.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/d57BdFh.png",
+    "url": "https://liveeu-gcp.alkassdigital.net/alkass3-p/main.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/8w61kFX.png",
+    "url": "https://liveeu-gcp.alkassdigital.net/alkass2-p/main.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://cdn.as.ro/wp-content/themes/antenasport/images/icons-as/as.svg",
+    "url": "https://hls.rundletv.eu.org/LIVE$AntenaSport/6.m3u8/Level/300720051?end=END&start=LIVE"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/XjzK3gZ.png",
+    "url": "https://stream-lb.livemediama.com/arryadia/hls/master.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/XjzK3gZ.png",
+    "url": "https://snrtworker.kharazbelaid.workers.dev/?id=arryadia.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.ibb.co/fYNx6Fk1/image-removebg-preview-1.png",
+    "url": "https://stream-lb.livemediama.com/arryadia-hd-01/hls/master.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.ibb.co/jPFCVFNS/image-removebg-preview-3.png",
+    "url": "https://stream-lb.livemediama.com/arryadia-hd-02/hls/master.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.ibb.co/mCnJzvvB/image-removebg-preview-4.png",
+    "url": "https://stream-lb.livemediama.com/arryadia-hd-03/hls/master.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/XjzK3gZ.png",
+    "url": "https://stream-lb.livemediama.com/arryadia-tnt/hls/master.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.ibb.co/bRmGbsyV/A3-SPORTTV.jpg",
+    "url": "https://streamtv.as3sport.online:3394/hybrid/play.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/bPWFXkL.png",
+    "url": "https://kanal75xto-llhls.akamaized.net/live/Data/atg-kanal-15-02a-rr/HLS-Legacy-HL/atg-kanal-15-02a-rr.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/MNw5cJH.jpg",
+    "url": "https://mgv-awapa.akamaized.net/hls/live/2104282/MGV_CHANNEL15/master.m3u8"
+  },
+  {
+    "name": "Unknown",
+    "logo": "https://i.imgur.com/fBpLsbC.png",
+    "url": "https://5c7b683162943.streamlock.net/live/ngrp:sportsone_all/playlist.m3u8"
+  }
+];
